07d76dd00d26e8de74c209a29cedf861d29e39f5
Mark <pivonosovm@gmail.com>
2020-08-22 16:58:17 +0300

    refactoring on OOP concept

--- src/classes/Block.js
+++ src/classes/Block.js
@@ -1,7 +1,64 @@
-export class Block {
-  constructor(type, value, options) {
-    this.type = type;
+import {col, row} from '../utils';
+
+class Block {
+  constructor(value, options) {
     this.value = value;
     this.options = options;
+  }
+
+  toHTML() {
+    throw new Error('Реализуй метод');
+  }
+}
+
+export class TitleBlock extends Block {
+  constructor(value, options) {
+    super(value, options);
+    this.tag = this.options.tag;
+    this.styles = this.options.styles;
+  }
+
+  toHTML() {
+
+    return row(col(`<${this.tag}>${this.value}</${this.tag}>`), this.styles);
+  }
+}
+
+export class ImageBlock extends Block {
+  constructor(value, options) {
+    super(value, options);
+  }
+
+  toHTML() {
+    const {alt, styles, imageStyles} = this.options;
+    const html = `<img src="${this.value}" alt="${alt}" style="${imageStyles}">`;
+    return row(html, styles);
+  }
+}
+
+export class TextColumnBlock extends Block {
+  constructor(value, options) {
+    super(value, options);
+    this.styles = options.styles;
+
+  }
+
+  toHTML() {
+    let map = this.value.map(item => col(item));
+    return row(map.join(''), this.styles);
+  }
+}
+
+export class TextBlock extends Block {
+  constructor(value, options) {
+    super(value, options);
+    this.tag = options.tag;
+    this.styles = options.styles;
+  }
+
+  toHTML() {
+    return row(
+        col(`<${this.tag} style="margin-bottom: 0;">${this.value}</${this.tag}>`),
+        this.styles);
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import {model} from './model';
-import {template} from './template';
 import './styles/main.css';
 
 const site = document.querySelector('#site');
 
 model.forEach(block => {
-  const generate = template[block.type];
-  if (generate) {
-    const html = generate(block);
-    site.insertAdjacentHTML('beforeend', html);
-  }
+  site.insertAdjacentHTML('beforeend', block.toHTML());
 });
